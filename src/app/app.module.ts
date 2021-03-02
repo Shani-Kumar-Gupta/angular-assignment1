6492115d8d1a28aa0cebb58c4593df392a99a18e
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-03-02 14:28:38 +0530

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayDetailsComponent } from './components/display-details/display-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayDetailsComponent
   ],
   imports: [
     BrowserModule,
